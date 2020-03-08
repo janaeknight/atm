a7b8d99377e9f0a93da18c260598c7c1adbd8401
Knight <janae.jpeg@gmail.com>
2020-03-07 18:22:23 -0600

    gen update

--- java/javascript.js
+++ java/javascript.js
@@ -1,3 +1,25 @@
 var email = "";
 var password  = "";
 var username = "";
+
+var logins = 0;
+
+$("#loginBtn").click(function() {
+
+    event.preventDefault();
+    console.log("Login attempted.");
+
+    if (logins<1) {
+        email = $("#emailInput").val();
+        password = $("#passInput").val();
+        console.log(email); console.log(password);
+        logins +=1; console.log(logins);
+        // console.log("Login attempt: SUCCESS");
+    } else {
+        if ($("#emailInput").val()===email && $("#passInput").val()===email) {
+            console.log("Login attempt: SUCCESS");
+        } else {
+            console.log("Login attempt: FAIL");
+        }
+    }
+});
